afe4b861821499ff8b1ad13f60daec1430e5a9eb
Martin Simpson <martinsimpson@Martins-MacBook-Pro.local>
2022-04-28 13:15:25 +0700

    Full tested storage

--- lib/storage/storage.rb
+++ lib/storage/storage.rb
@@ -1,24 +1,26 @@
 module GenCache
     class Storage
-        def initialize(config:, namespace:)
-            @driver = GenCache.configuration.storage_driver.instance
+        def initialize(config:, namespace:, global_config: GenCache.configuration, logger: GenCache, wrapper: Wrapper)
+            @logger = logger
+            @wrapper = wrapper
+            @driver = global_config.storage_driver.instance
             @driver.set_config(config: config)
             @namespace = namespace
-            GenCache.log :debug, "storage", "initialized storage with driver: #{@driver} in namespace: #{@namespace}"
+            @logger.log :debug, "storage", "initialized storage with driver: #{@driver} in namespace: #{@namespace}"
         end
 
         def get item_id
-            GenCache.log :debug, "storage", "get #{item_id}"
+            @logger.log :debug, "storage", "get #{item_id}"
             @driver.get(item_id, namespace: @namespace)
         end
 
         def set id, item
-            GenCache.log :debug, "storage", "set #{id} #{item}"
-            @driver.set(id, Wrapper.wrap(id, item), namespace: @namespace)
+            @logger.log :debug, "storage", "set #{id} #{item}"
+            @driver.set(id, @wrapper.wrap(id, item), namespace: @namespace)
         end
 
         def delete item_id
-            GenCache.log :debug, "storage", "delete #{item_id}"
+            @logger.log :debug, "storage", "delete #{item_id}"
             @driver.delete(item_id, namespace: @namespace)
         end
     end
--- test/gencache_test.rb
+++ test/gencache_test.rb
@@ -8,6 +8,8 @@ require "lib/gencache.rb"
 require "lib/error.rb"
 require "lib/cache.rb"
 
+require "lib/storage/storage.rb"
+
 describe GenCache do
     describe ".with_cache" do
         let(:cache_name) { "test_cache" }
@@ -142,5 +144,94 @@ describe GenCache::Cache do
             cache.delete cache_item_id
         end
     end
+
+end
+
+describe GenCache::Storage do
+    let(:config) { {} }
+    let(:namespace) { "namespace" }
+    let(:global_config) { Minitest::Mock.new }
+    let(:logger) { Minitest::Mock.new }
+    let(:storage_driver) { Minitest::Mock.new }
+
+    describe ".initialize" do
+        it "should call the driver" do
+            global_config.expect :storage_driver, storage_driver
+            storage_driver.expect :instance, storage_driver
+            storage_driver.expect :set_config, nil, [{ config: config}]
+            logger.expect :log, true, [Object, Object, Object]
+            GenCache::Storage.new(config: config, namespace: namespace, global_config: global_config, logger: logger, wrapper: nil)
+
+            assert_mock global_config
+            assert_mock storage_driver
+            assert_mock logger
+        end
+    end
+
+    describe ".get" do
+        before do
+            global_config.expect :storage_driver, storage_driver
+            storage_driver.expect :instance, storage_driver
+            storage_driver.expect :set_config, nil, [{ config: config}]
+            logger.expect :log, true, [Object, Object, Object]
+        end
+
+        let(:storage) { GenCache::Storage.new(config: config, namespace: namespace, global_config: global_config, logger: logger, wrapper: nil) }
+
+        let(:item_id) { "abcdef" }
+        it "should call the driver" do
+            logger.expect :log, true, [Object, Object, Object]
+            storage_driver.expect :get, nil, [item_id, {namespace: namespace}]
+            storage.get(item_id)
+
+            assert_mock storage_driver
+            assert_mock logger
+        end
+    end
+
+    describe ".set" do
+        before do
+            global_config.expect :storage_driver, storage_driver
+            storage_driver.expect :instance, storage_driver
+            storage_driver.expect :set_config, nil, [{ config: config}]
+            logger.expect :log, true, [Object, Object, Object]
+        end
+        let(:item_id) { "abcdef" }
+        let(:item) { "something" }
+        let(:wrapper) { Minitest::Mock.new }
+        let(:storage) { GenCache::Storage.new(config: config, namespace: namespace, global_config: global_config, logger: logger, wrapper: wrapper) }
+
+        it "should call the driver" do
+            logger.expect :log, true, [Object, Object, Object]
+            storage_driver.expect :set, nil, [item_id, item, { namespace: namespace}]
+            wrapper.expect :wrap, item, [item_id, item]
+            storage.set(item_id, item)
+
+            assert_mock storage_driver
+            assert_mock logger
+        end
+    end
+
+    describe ".delete" do
+        before do
+            global_config.expect :storage_driver, storage_driver
+            storage_driver.expect :instance, storage_driver
+            storage_driver.expect :set_config, nil, [{ config: config}]
+            logger.expect :log, true, [Object, Object, Object]
+        end
+
+        let(:storage) { GenCache::Storage.new(config: config, namespace: namespace, global_config: global_config, logger: logger, wrapper: nil) }
+
+        let(:item_id) { "abcdef" }
+        it "should call the driver" do
+            logger.expect :log, true, [Object, Object, Object]
+            storage_driver.expect :delete, nil, [item_id, {namespace: namespace}]
+            storage.delete(item_id)
+
+            assert_mock storage_driver
+            assert_mock logger
+        end
+    end
+
 
 end
